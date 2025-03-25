c4249be30847a3669be57c2fa24f628904bc835a
KonstantinRTS <k.developer@acceptance.ltd>
2025-03-25 11:57:24 +0200

    all files 25

--- src/app/recruitment-hiring-services/_components/ServicesRequest.jsx
+++ src/app/recruitment-hiring-services/_components/ServicesRequest.jsx
@@ -25,7 +25,7 @@ const ServicesRequest = () => {
                         variants={fadeInUp}
                         custom={0.3}
                         className="services-request__text">Get a detailed breakdown of our Recruitment & Hiring Services and discover the best solution for your workforce needs.</motion.p>
-                    <Link href="#" download className="services-request__link">
+                    <Link href="/Neon_hr_group_price_list.pdf" target="_blank" download className="services-request__link">
                         Download Full Price List
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 16L7 11L8.4 9.55L11 12.15V4H13V12.15L15.6 9.55L17 11L12 16ZM4 20V15H6V18H18V15H20V20H4Z" fill="white" />
