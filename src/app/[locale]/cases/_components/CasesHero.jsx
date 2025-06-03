3de8ddab62637a605b6935637cd1911f04dbfe79
daniil-b2be <dan.developer@born2be.media>
2025-06-03 20:22:21 +0300

    cases page translations

--- src/app/[locale]/cases/_components/CasesHero.jsx
+++ src/app/[locale]/cases/_components/CasesHero.jsx
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from "react";
 import "@/styles/cases.scss";
 import { fadeInUp } from "@/utils/animations";
 import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
 
 const CasesHero = () => {
+    const t = useTranslations("cases.hero");
 
     return (
         <section className="cases-hero">
@@ -16,9 +18,10 @@ const CasesHero = () => {
                         viewport={{ once: true }}
                         variants={fadeInUp}
                         custom={0.2}
-                        className="cases-hero__title">
-                        <span>HR Success Stories</span> <br />
-                        Real Cases, Real Impact
+                        className="cases-hero__title"
+                    >
+                        <span>{t("title.0")}</span> <br />
+                        {t("title.1")}
                     </motion.h1>
                     <motion.p
                         initial="hidden"
@@ -26,7 +29,10 @@ const CasesHero = () => {
                         viewport={{ once: true }}
                         variants={fadeInUp}
                         custom={0.3}
-                        className="cases-hero__text">Strategic HR moves that transformed businesses.</motion.p>
+                        className="cases-hero__text"
+                    >
+                        {t("text")}
+                    </motion.p>
                 </div>
             </div>
         </section>
